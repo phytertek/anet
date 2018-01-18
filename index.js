0036d13b72b0cad9390d98d6e64caa46dcdf35a6
phytertek <ryan.phytertek@gmail.com>
2018-01-17 22:31:19 -0500

    removal process

--- index.js
+++ index.js
@@ -9,18 +9,27 @@ const port = process.argv[2] || process.env.PORT; // 3000;
 const host = `${hostName}`;
 
 let network = [...new Set([origin, host])];
-
-const nextNeighbor = () => {
+const removeNode = (node) => {
+  const newNet = new Set(network);
+  newNet.delete(n);
+  network = [...newNet];
+}
+const nextNode = () => {
   network = [...network];
   const next = network.shift();
   network.push(next);
   network = [...new Set(network)];
   return next;
 };
-const removeNeighbor = n => {
-  const newNet = new Set(network);
-  newNet.delete(n);
-  network = [...newNet];
+const networkRemoveNode = async n => {
+  try {
+    removeNode(n)
+    network.forEach(node => {
+      await axios.post(`${net}remove-node`, { node: n })
+    })
+  } catch (error) {
+    console.log(error)
+  }
 };
 const mergeNetwork = n => {
   network = [...new Set([...network, ...n])];
@@ -33,14 +42,16 @@ const pollRun = async () => {
   try {
     clearInterval(poller);
     await checkNeighbor();
-  } catch (error) {}
+  } catch (error) {
+    console.log(error)
+  }
 };
 
 let next;
 
 const checkNeighbor = async () => {
   try {
-    next = nextNeighbor();
+    next = nextNode();
     console.log(next);
     if (next !== host) {
       const nextNeighborResponse = await httpReq.post(`${next}check`, {
@@ -51,7 +62,7 @@ const checkNeighbor = async () => {
     poller = setInterval(pollRun, 1000);
   } catch (error) {
     console.log('error', next);
-    removeNeighbor(next);
+    await networkRemoveNode(next);
     poller = setInterval(pollRun, 1000);
   }
 };
@@ -69,6 +80,8 @@ server.post('/check', async (req, res) => {
     res.json(error);
   }
 });
+
+server.post('/remove-node', async)
 server.listen(port, err => {
   if (err) return console.log(err);
   console.log('Server running on port', port);
